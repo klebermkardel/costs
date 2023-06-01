4bbe2784486390b306896952677c69e6a2e37261
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-01 17:06:58 -0300

    Estruturando o projeto - Cria pasta components>layout e dentro dela cria componente Container.js para abrigar o conteúdo principal de todas as páginas

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 
+import Container from './components/layout/Container'
+
 
 function App() {
   return (
-
     <Router>
       <div>
         <Link to='/'>Home</Link>
@@ -16,12 +17,14 @@ function App() {
         <Link to='/company'>Empresa</Link>
         <Link to='/newproject'>Novo Projeto</Link>
       </div>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/company' element={<Company />} />
-        <Route exact path='/contact' element={<Contact />} />
-        <Route exact path='/newproject' element={<NewProject />} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/company' element={<Company />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/newproject' element={<NewProject />} />
+        </Routes>
+        </Container>
       <footer>Footer</footer>
     </Router>
   );
