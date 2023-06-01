9b99f48dd9119ba0db53fa49ed92e8ac889e42e3
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-01 16:54:46 -0300

    Estruturando o projeto - Instala versão mais recente do react-router-dom e troca Switch por Routes pois Switch não é mais utilizada na nova versão

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
@@ -8,28 +8,21 @@ import NewProject from './components/pages/NewProject'
 
 function App() {
   return (
+
     <Router>
       <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/newproject'>Novo Projeto</Link>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/company">
-          <Company />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-        <Route exact path="/newproject">
-          <NewProject />
-        </Route>
-      </Switch>
-      <p>Footer</p>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/company' element={<Company />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route exact path='/newproject' element={<NewProject />} />
+      </Routes>
+      <footer>Footer</footer>
     </Router>
   );
 }
