6666f7911971701431847f7dbce20de551cc23db
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-02 08:34:05 -0300

    Estruturando o projeto - Cria componentes de Navbar e Footer dentro da pasta components>layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
@@ -6,17 +6,15 @@ import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
 
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/contact'>Contato</Link>
-        <Link to='/company'>Empresa</Link>
-        <Link to='/newproject'>Novo Projeto</Link>
-      </div>
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
           <Route exact path='/' element={<Home />} />
@@ -24,8 +22,8 @@ function App() {
           <Route exact path='/contact' element={<Contact />} />
           <Route exact path='/newproject' element={<NewProject />} />
         </Routes>
-        </Container>
-      <footer>Footer</footer>
+      </Container>
+      <Footer />
     </Router>
   );
 }
