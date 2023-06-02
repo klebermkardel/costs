7b33f2176db14267e353c9045eec15c2b3ec1cc5
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-02 10:03:02 -0300

    Estruturando o projeto - Estiliza componente Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ import savings from '../../img/savings.svg'
 
 function Home() {
     return (
-        <section>
+        <section className={styles.home_container}>
             <h1>Bem vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
             <a href="/">Criar Projeto</a>
