7590d5e6684253ac6ca2b62e1b996ae774533dcf
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-05 12:58:52 -0300

    Estruturando backend - Utiliza UseEffect e cria request com API para trazer resultado de categories do banco e usar nas options do select

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Input from '../form/Input'
 import Select from '../form/Select'
@@ -10,17 +10,19 @@ function ProjectForm({btnText}) {
 
     const [categories, setCategories] = useState([])
 
-    fetch("http://localhost:5000/categories", {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then((resp) => resp.json())
-    .then((data) => {
-        setCategories(data)
-    })
-    .catch((err) => console.log(err))
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+         .then((resp) => resp.json())
+         .then((data) => {
+            setCategories(data)
+         })
+         .catch((err) => console.log(err))
+    }, [])
 
     return (
         <form className={styles.form}>
@@ -36,7 +38,9 @@ function ProjectForm({btnText}) {
                 name="budget"
                 placeholder="Insira o orçamento total"
             />
-            <Select name="category_id" text="Selecione a categoria" />
+            <Select 
+                name="category_id" text="Selecione a categoria"
+                options={categories} />
             <SubmitButton text={btnText}/>
         </form>
     )
