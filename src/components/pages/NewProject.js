b00d09fe8337c40db11b82df68fb3b7ad7531531
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-05 10:33:16 -0300

    Estruturando o projeto - Cria estilização do componente NewProject.js

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -2,7 +2,7 @@ import styles from './NewProject.module.css'
 
 function NewProject() {
     return (
-        <div>
+        <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois aidicionar os serviços</p>
             <p>Formulário</p>
