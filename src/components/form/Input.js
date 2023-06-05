03123912c70ecf3976eac3ab5f731c9f237a4284
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-05 10:59:23 -0300

    Estruturando o projeto - Deixa os dados dinâmicos baseado nas props passadas no componente

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -3,8 +3,14 @@ import styles from './Input.module.css'
 function Input({ type, text, name, placeholder, handleOnChange, value}) {
     return (
         <div className={styles.form_control}>
-            <label htmlFor=""></label>
-            <input type="" />
+            <label htmlFor={name}>{text}:</label>
+            <input
+                type={type}
+                name={name}
+                id={name}
+                placeholder={placeholder}
+                onChange={handleOnChange}
+                value={value} />
         </div>
     )
 }
