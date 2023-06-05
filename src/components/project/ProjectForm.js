46155c3631abec653ad02664dfbc284dfed19a69
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-05 11:05:52 -0300

    Estruturando o projeto - Importa componente Input.js dentro de ProjectForm.js

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,14 +1,22 @@
+import Input from '../form/Input'
+
 import styles from './ProjectForm.module.css'
 
 function ProjectForm() {
     return (
         <form className={styles.form}>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto" />
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
+            <Input
+                type="text"
+                text="Nome do Projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"
+            />
+            <Input 
+                type="text"
+                text="Orçamento do Projeto"
+                name="budget"
+                placeholder="Insira o orçamento total"
+            />
             <div>
                 <select name="category_id">
                     <option disabled selected>Selecione a categoria</option>
