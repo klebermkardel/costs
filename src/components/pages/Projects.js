beb74c6cf738868749d6be8e802a350f16e54746
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-07 16:54:20 -0300

    Estruturando o projeto - Cria mensagem de remoção do projeto

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -13,6 +13,7 @@ import styles from './Projects.module.css'
 function Projects() {
   const [projects, setProjects] = useState([])
   const [removeLoading, setRemoveLoading] = useState(false)
+  const [projectMessage, setProjectMessage] = useState('')
 
   const location = useLocation()
   let message = ''
@@ -47,7 +48,7 @@ function Projects() {
       .then((resp) => resp.json())
       .then((data) => {
         setProjects(projects.filter((project) => project.id !== id))
-        // message
+        setProjectMessage('Projeto removido com sucesso!')
       })
       .catch((err) => console.log(err))
   }
@@ -59,6 +60,7 @@ function Projects() {
           <LinkButton to="/newproject" text="Criar Projeto" />
         </div>
         {message && <Message type="success" msg={message} />}
+        {projectMessage && <Message type="success" msg={projectMessage} />}
         <Container customClass="start">
           {projects.length > 0 && 
             projects.map((project) => (
