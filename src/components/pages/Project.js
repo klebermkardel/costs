2bac7a4035c7eaa50400a996b743fb631a15c4c6
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-07 19:32:03 -0300

    Estruturando o projeto - Cria área de serviços dentro da página de edição do projeto

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -14,6 +14,8 @@ function Project() {
 
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
+
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -34,6 +36,8 @@ function Project() {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
+
         // budget validation
         if (project.budget <= project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto')
@@ -60,6 +64,10 @@ function Project() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
 
     return (
@@ -91,6 +99,23 @@ function Project() {
                             </div>
                         )}
                     </div>
+                    <div className={styles.service_form_container}>
+                        <h2>Adicione um serviço:</h2>
+                        <button className={styles.btn} onClick={toggleServiceForm}>
+                            {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                        </button>
+                        <div className={styles.project_info}>
+                            {showServiceForm && 
+                            <div>
+                                Formulário do serviço
+                            </div>
+                            }
+                        </div>
+                    </div>
+                    <h2>Serviços</h2>
+                    <Container customClass="start">
+                        <p>Itens de serviços</p>
+                    </Container>
                 </Container>
             </div>
             : <Loading />}
