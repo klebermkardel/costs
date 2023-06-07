512b05521e342ca000c4752c54b877dbb327cbd0
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-07 18:15:39 -0300

    Estruturando o projeto - Cria estrutura JSX da página de edição dos projetos

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -4,12 +4,14 @@ import { useParams } from 'react-router-dom'
 import {useState, useEffect} from 'react'
 
 import Loading from '../layout/Loading'
+import Container from '../layout/Container'
 
 function Project() {
 
     const { id } = useParams()
 
     const [project, setProject] = useState([])
+    const [showProjectForm, setShowProjectForm] = useState(false)
 
     useEffect(() => {
       setTimeout(() => {
@@ -27,9 +29,41 @@ function Project() {
       }, 500)
     }, [id])
 
+    function toggleProjectForm() {
+        setShowProjectForm(!showProjectForm)
+    }
+
     return (
         <>
-            {project.name ? (<p>{project.name}</p>) : (<Loading />)}
+            {project.name ? 
+            <div>
+                <Container customClass="column">
+                    <div>
+                        <h1>Projeto: {project.name}</h1>
+                        <button onClick={toggleProjectForm}>
+                            {!showProjectForm ? 'Editar Projeto' : 'Fechar'}
+                        </button>
+                        {!showProjectForm ? (
+                            <div>
+                                <p>
+                                  <span>Categoria:</span> {project.category.name}
+                                </p>
+                                <p>
+                                  <span>Total do Orçamento:</span> R$ {project.budget}
+                                </p>
+                                <p>
+                                  <span>Total Utilizado:</span> R$ {project.cost}
+                                </p>
+                            </div>
+                        ) : (
+                            <div>
+                                <p>Detalhes do Projeto</p>
+                            </div>
+                        )}
+                    </div>
+                </Container>
+            </div>
+            : <Loading />}
         </>
     )
 }
