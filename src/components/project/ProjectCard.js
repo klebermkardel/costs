07fa8cd69d7339400606b1db2781c6663553c07a
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-07 09:56:07 -0300

    Estruturando o projeto - Finaliza estrutura base do ProjectCard

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -4,7 +4,19 @@ import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
 
 function ProjectCard({id, name, budget, category, handleRemove}) {
     return (
-        <p>{name}</p>
+        <div>
+            <h4>{name}</h4>
+            <p>
+                <span>Orçamento: </span> R$ {budget}
+            </p>
+            <p>
+                <span></span> {category}
+            </p>
+            <div>
+                <p>Editar</p>
+                <p>Remover</p>
+            </div>
+        </div>
     )
 }
 
