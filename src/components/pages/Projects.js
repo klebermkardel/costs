3ba2fb29dd61b906e36518608772bef825abd9e1
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-07 09:51:23 -0300

    Estruturando o projeto - Passa todas as propriedades do ProjectCard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -43,7 +43,13 @@ function Projects() {
         <Container customClass="start">
           {projects.length > 0 && 
             projects.map((project) => (
-              <ProjectCard name={project.name} />
+              <ProjectCard
+                id={project.id} 
+                name={project.name} 
+                budget={project.budget}
+                category={project.category.name}
+                key={project.id}
+              />
             ))}
         </Container>
     </div>
