83b419013d577e72575439da8141df35657e1edc
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-07 16:34:48 -0300

    Estruturando o projeto - Cria exibição da mensagem de que não há projetos cadastrados quando for o caso

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -56,6 +56,9 @@ function Projects() {
               />
             ))}
             {!removeLoading && <Loading />}
+            {removeLoading && projects.length === 0 && (
+              <p>Não há projetos cadastrados</p>
+            )}
         </Container>
     </div>
   )
