f98ab305f463ed125df902ce70272136415c00d4
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-08 10:05:36 -0300

    Resolvendo bugs - Corrige alguns componentes que atualizaram na última versão do react-router-dom

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -5,7 +5,7 @@ import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject() {
-  const history = useNavigate()
+  const navigate = useNavigate()
 
   function createPost(project) {
     // initialize cost and services
@@ -21,7 +21,7 @@ function NewProject() {
     })
       .then((resp) => resp.json())
       .then((data) => {
-        history('/projects', { state:  { message: 'Projeto criado com sucesso!' } })
+        navigate('/projects', { state:  { message: 'Projeto criado com sucesso!' } })
       })
   }
 
